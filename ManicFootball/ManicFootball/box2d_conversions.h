6997c19f253804f125b481ede3dd15bae657319e
jmottershead94 <jasonmottershead@hotmail.co.uk>
2015-10-24 14:52:39 +0100

    Adding in the Box2D physics library.

---
+++ ManicFootball/ManicFootball/box2d_conversions.h
@@ -0,0 +1,23 @@
+// This header file helps to convert box2d coordinates into local framework coordinates.
+
+#ifndef _BOX2D_CONVERSIONS_H
+#define _BOX2D_CONVERSIONS_H
+
+#define DISPLAY_WIDTH				1280
+#define DISPLAY_HEIGHT				720
+
+// To convert the Box2D coordinates to framework coordinates.
+#define BOX2D_FRAMEWORK_SCALE 100.0f
+#define BOX2D_FRAMEWORK_OFFSET_X 0.0f
+#define BOX2D_FRAMEWORK_OFFSET_Y 0.0f
+
+#define BOX2D_FRAMEWORK_POS_X(x) ((x)*BOX2D_FRAMEWORK_SCALE + BOX2D_FRAMEWORK_OFFSET_X)
+#define BOX2D_FRAMEWORK_POS_Y(y) (DISPLAY_HEIGHT - ((y)*BOX2D_FRAMEWORK_SCALE + BOX2D_FRAMEWORK_OFFSET_Y))
+#define BOX2D_FRAMEWORK_SIZE(s) ((s)*BOX2D_FRAMEWORK_SCALE)
+
+// To convert the framework coordinates to Box2D coordinates.
+#define FRAMEWORK_BOX2D_POS_X(x) (((x) - BOX2D_FRAMEWORK_OFFSET_X)/BOX2D_FRAMEWORK_SCALE)
+#define FRAMEWORK_BOX2D_POS_Y(y) (((y) - DISPLAY_HEIGHT+BOX2D_FRAMEWORK_OFFSET_Y)/-BOX2D_FRAMEWORK_SCALE)
+#define FRAMEWORK_BOX2D_SIZE(s) ((s)/BOX2D_FRAMEWORK_SCALE)
+
+#endif
---
+++ ManicFootball/ManicFootball/game_object.h
@@ -0,0 +1,14 @@
+#ifndef _GAME_OBJECT_H_
+#define _GAME_OBJECT_H_
+
+class GameObject
+{
+
+	public:
+		// Methods.
+		GameObject();
+		~GameObject();
+
+};
+
+#endif
