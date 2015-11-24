31c652207ce200d9875d03e19f23ca667b894526
jmottershead94 <jasonmottershead@hotmail.co.uk>
2015-11-24 10:11:08 +0000

    Taken out pointless bool value.

--- ManicFootball/Client/ManicFootball/game_object.cpp
+++ ManicFootball/Client/ManicFootball/game_object.cpp
@@ -17,7 +17,6 @@ void GameObject::Init(sf::Vector2f position, sf::Vector2f dimensions, ObjectID o
 	respawn_position_ = position;	// Setting the intial respawn position of the object.
 	dimension_ = dimensions;		// Setting the width and height of the game object shape.
 	id_ = object_id;				// Setting the id number of the game object shape.
-	on_surface_ = false;			// Letting us know if the game object is on a surfact or not.
 	remove_ = false;				// Letting us know if the game object needs to be removed or not.
 	is_rectangle_ = is_rectangle;	// Letting us know what kind of shape we have.
 
